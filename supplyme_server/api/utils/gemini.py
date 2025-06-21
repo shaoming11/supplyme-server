1cdd4b535f46aa0b6406aba4db6a77eefd3f2ace
Seyon Satheesh <seyon.satheesh@gmail.com>
2025-06-21 17:40:03 -0400

    Utils

---
+++ supplyme_server/api/utils/gemini.py
@@ -0,0 +1,11 @@
+def generateEmbedding(text):
+    pass
+
+def findHSCode(text):
+    pass
+
+def generateCompanyDescription(campanyName):
+    pass
+
+def generateSupplierDescription(campanyName):
+    pass
---
+++ supplyme_server/api/utils/mongodb.py
@@ -0,0 +1,11 @@
+def addCompany(companyName, companyDescription):
+    pass
+
+def addSupplier(companyID, supplierName, supplierDescription):
+    pass
+
+def findBestCompanies(prompt, numberOfCompanies):
+    pass
+
+def findBestSuppliers(prompt, companyID, numberOfSuppliers):
+    pass
