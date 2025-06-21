4eda07dabcfa710637c5f9289800575959db3cbc
Seyon Satheesh <seyon.satheesh@gmail.com>
2025-06-21 19:22:48 -0400

    MONGO DB UPDATE

--- supplyme_server/api/utils/mongodb.py
+++ supplyme_server/api/utils/mongodb.py
@@ -1,8 +1,29 @@
-def addCompany(companyName, companyDescription):
-    pass
+from mongoengine import *
+from .gemini import *
+import os
 
-def addSupplier(companyID, supplierName, supplierDescription):
-    pass
+connect(host=os.getenv('ATLAS_URI'))
+
+class Company(Document):
+    name = StringField(required=True)
+    description = StringField(required=True)
+    country = StringField(required=True)
+    embedding = ListField(FloatField())
+
+class Supplier(Document):
+    name = StringField(required=True)
+    description = StringField(required=True)
+    country = StringField(required=True)
+    embedding = ListField(FloatField())
+    company = ReferenceField(Company, reverse_delete_rule=CASCADE)
+
+def addCompany(companyName, companyCountry):
+    companyDescription = generateCompanyDescription(f"{companyName} ({companyCountry})")
+    company = Company(name=companyName, companyDescription=companyDescription, embedding=generateEmbedding(f"{companyName} ({companyCountry}):\n\n {companyDescription}")).save()
+
+def addSupplier(companyID, supplierName, supplierCountry):
+    supplierDescription = generateSupplierDescription(f"{supplierName} ({supplierCountry})")
+    supplier = Supplier(name=supplierName, supplierDescription=supplierDescription, embedding=generateEmbedding(f"{supplierName} ({supplierCountry}):\n\n {supplierDescription}"), company=companyID).save()
 
 def findBestCompanies(prompt, numberOfCompanies):
     pass
