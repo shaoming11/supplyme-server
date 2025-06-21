74f1d0acc1a84d34558ca91dad339cdd79117c6a
shao <68085490+shaoming11@users.noreply.github.com>
2025-06-21 17:42:36 -0400

    update

--- supplyme_server/api/views.py
+++ supplyme_server/api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from google import genai
+from dotenv import load_dotenv
 
-# Create your views here.
+import os
+
+load_dotenv() # Load variables from .env
+
+api_key = os.getenv('API_KEY')
+database_url = os.getenv('ATLAS_URI')
+
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
+)
+
+def index(request):
+    print(response)
+    return render(request)
