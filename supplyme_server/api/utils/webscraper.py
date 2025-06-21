0adf6c8a063b1da79250f7c184e2392bf4e8d09e
shao <68085490+shaoming11@users.noreply.github.com>
2025-06-21 19:22:46 -0400

    scrape

---
+++ supplyme_server/api/utils/webscraper.py
@@ -0,0 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+soup = BeautifulSoup("<p>Some<b>bad<i>HTML", "html.parser")
+results = soup.find(id="ResultsContainer")
+suppliers = results.find_all("div", class_="card-content")
+# text-yeti-abominable-10 hover:underline fill-blue-700 font-bold text-sm
