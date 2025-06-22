98db0eeb24b9c1a38b2f55eef7bcfae96e2ceec8
Seyon Satheesh <seyon.satheesh@gmail.com>
2025-06-22 05:15:36 -0400

    Build Update

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+cd supplyme_server
+
 # Convert static asset files
 python manage.py collectstatic --no-input
 
