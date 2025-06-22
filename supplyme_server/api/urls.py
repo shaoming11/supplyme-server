c9ea917cacb2a4acc72b97d2d798b5894471dce4
Seyon Satheesh <seyon.satheesh@gmail.com>
2025-06-22 04:53:09 -0400

    Views Update

--- supplyme_server/api/urls.py
+++ supplyme_server/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.GetResults.as_view(), name="index")
+    path("api", views.GetResults.as_view(), name="api")
 ]
