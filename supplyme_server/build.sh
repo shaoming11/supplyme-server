a2db2c61f193fed0bdc58c5bc246063b9d098c53
Seyon Satheesh <seyon.satheesh@gmail.com>
2025-06-22 06:07:37 -0400

    Production #3

--- supplyme_server/build.sh
+++ supplyme_server/build.sh
@@ -5,8 +5,6 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-cd supplyme_server
-
 # Convert static asset files
 python manage.py collectstatic --no-input
 
